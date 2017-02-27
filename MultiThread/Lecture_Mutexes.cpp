f74d983d60862ac48dcea6babb2c1e72f5b496ea
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-02-27 10:39:45 +0000

    after mutex

--- MultiThread/Lecture_Mutexes.cpp
+++ MultiThread/Lecture_Mutexes.cpp
@@ -7,7 +7,7 @@ int terminate_app = 0;
 
 char    key = 'a'; // Global data
 
-HANDLE  mutexhdl;
+
 LPCTSTR mutexname = (LPCTSTR)"FirstMutex";
 
 
@@ -15,25 +15,37 @@ DWORD WINAPI ThreadProc(LPVOID lpParameter)
 {
 	HANDLE  mutexhdl;
 
+	mutexhdl = OpenMutex(MUTEX_ALL_ACCESS, FALSE, mutexname);
+
 	while (1)
 	{
+		WaitForSingleObject(mutexhdl, INFINITE);
 		printf("Thread %c\r\n", key);
+		ReleaseMutex(mutexhdl);
 		if (terminate_app)
 			break;
+
+		Sleep(0);
 	}
 
+	CloseHandle(mutexhdl);
 	return 0;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	HANDLE  mutexhdl;
 	HANDLE  threadhdl;
+
+	mutexhdl = CreateMutex(NULL, FALSE, mutexname); 
 
 	threadhdl = CreateThread(NULL, 0, ThreadProc, (LPVOID)0, 0, NULL);
 
 	while (key != 'q')
 	{
+		WaitForSingleObject(mutexhdl, INFINITE);
 		key = _getch();
+		ReleaseMutex(mutexhdl);
 	}
 
 	terminate_app = 1;
@@ -41,6 +53,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	WaitForSingleObject(threadhdl, INFINITE);
 
 	CloseHandle(threadhdl);
+	CloseHandle(mutexhdl);
 
 	//getchar();
 	return 0;
